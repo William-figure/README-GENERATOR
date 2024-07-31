447c617b68269ecdcaadc3a04dd6db119c1d32ea
William-figure <wyc19981230@outlook.com>
2024-07-31 15:48:53 +1000

    successfully setup the inquirer package

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer')
 // TODO: Create an array of questions for user input
-const questions = [];
+// const questions = [];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -11,3 +11,15 @@ function init() {}
 
 // Function call to initialize app
 init();
+
+
+// a test for setup
+// const questions = [{
+//   type: 'input',
+//   name: 'name',
+//   message: "What's your name?"
+// }];
+
+// inquirer.prompt(questions).then(answers => {
+//   console.log(`Hello, ${answers.name}!`);
+// });
