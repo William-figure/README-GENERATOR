9e32cb9904b4f69f53fd6de03175206b5e0e567a
William-figure <wyc19981230@outlook.com>
2024-07-31 20:54:14 +1000

    add function for redering the content of the readme file

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -30,8 +30,64 @@ function renderLicenseSection(license) {
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  
+  ${renderLicenseBadge(data.license)}
 
-`;
+  ## Description
+
+  ${data.description}
+
+  - **Motivation:** ${data.motivation}
+  - **Why did you build this project?:** ${data.buildReason}
+  - **What problem does it solve?:** ${data.problem}
+  - **What did you learn?:** ${data.learning}
+
+  ## Table of Contents
+
+  - [Installation](#installation)
+  - [Usage](#usage)
+  ${data.includeCredits ? '- [Credits](#credits)' : ''}
+  - ${renderLicenseLink(data.license)}
+  ${data.includeBadges ? '- [Badges](#badges)' : ''}
+  ${data.includeFeatures ? '- [Features](#features)' : ''}
+  ${data.includeContribute ? '- [How to Contribute](#how-to-contribute)' : ''}
+  ${data.includeTests ? '- [Tests](#tests)' : ''}
+
+  ## Installation
+
+  ${data.installation}
+
+  ## Usage
+
+  ${data.usage}
+
+  \'\'\'
+  ![Screenshot]($(data.screenshot))
+  \'\'\'
+
+  ${data.includeCredits ? `## Credits
+
+  List your collaborators with links to their GitHub profiles: ${data.credits}` : ''}
+
+  ${rederLicenseSection(data.license)}
+
+  ${data.includeBadges ? `## Badges
+
+  ${data.badges}` : ''}
+
+  ${data.includeFeatures ? `## Features
+
+  ${data.features}` : '' }
+
+  ${data.includeContribute ? `## How to Constribute
+
+  ${data.contribute}` : ''}
+
+  ${data.includeTests ? `## Tests
+
+  ${data.tests}` : ''}
+
+  `;
 }
 
 module.exports = generateMarkdown;
